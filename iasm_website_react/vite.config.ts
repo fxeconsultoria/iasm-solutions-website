d15a0b6dc4502efcb0a64f95ee3edfa8a710ff57
fxeconsultoria <fxeconsultoria@gmail.com>
2025-06-03 09:55:35 -0300

    Update vite.config.ts

--- iasm_website_react/vite.config.ts
+++ iasm_website_react/vite.config.ts
@@ -3,11 +3,16 @@ import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  plugins: [react()],
+  import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/iasm-solutions-website/',
+  plugins: [react( )],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
+  }
 })
-
